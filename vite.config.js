395181ffb314bb9166a6b07f95e001a83f930a7d
SivaKarri <sivaakarri@gmail.com>
2025-09-05 21:45:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
   export default defineConfig({
-  base: "/Siya_Elite_Realty/",
+  base: "/siya_elite_realty/",
   plugins: [react()],
 })
